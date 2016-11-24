ca4d32feec66215cc9f097bc1af72fe0bc33a072
zimonwz <simondk123@gmail.com>
2016-11-24 14:54:29 +0100

    Change of HomePage and Header.
    Header color to blue and text to white.

--- GakuenMVC/Controllers/SchedulesController.cs
+++ GakuenMVC/Controllers/SchedulesController.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using DLLGakuen.Context;
+//using DLLGakuen.Context;
 using DLLGakuen.Entity;
 
 namespace GakuenMVC.Controllers
