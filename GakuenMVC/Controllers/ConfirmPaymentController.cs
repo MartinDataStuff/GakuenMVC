d8c45d9268aade290fa2533e2308799b6bbd1175
LifeLich <simo770g@easv365.dk>
2016-12-01 12:37:04 +0100

    Fixed Shop stuff

--- GakuenMVC/Controllers/ConfirmPaymentController.cs
+++ GakuenMVC/Controllers/ConfirmPaymentController.cs
@@ -11,6 +11,7 @@ namespace GakuenMVC.Controllers
     public class ConfirmPaymentController : Controller
     {
         private readonly IServiceGateway<User> _UserServiceGateway = new DllFacade().GetUserServiceGateway();
+        //private readonly IServiceGateway<Product> _ProductServiceGateway = new DllFacade().GetProductServiceGateway();
         // GET: ConfirmPayment
         public ActionResult Index()
         {
@@ -40,11 +41,17 @@ namespace GakuenMVC.Controllers
 
         }
         // this is just for test 
-        private OrderList itemss = new OrderList()
+        private OrderList Products = new OrderList()
         {
             ItemsList = new List<Product>()
             {
-                new Product() { Id = 1, Price = 50, Info = "Stay the Week", Name = "Week"},
+                new Product() { Id = 1, Price = 510, Info = "Stay the Week", Name = "Week"},
+                new Product() { Id = 2, Price = 520, Info = "get a sleep place", Name = "Sleep"},
+                new Product() { Id = 3, Price = 530, Info = "Stawdsk", Name = "something"},
+                new Product() { Id = 4, Price = 5540, Info = "Stayqwek", Name = "bibo"},
+                new Product() { Id = 5, Price = 5650, Info = "Staqweek", Name = "lord of the ring"},
+                new Product() { Id = 6, Price = 560, Info = "StaqweWeek", Name = "coldplay show"},
+                new Product() { Id = 7, Price = 51231230, Info = "Stayqweek", Name = "Death or manga"}
             }
 
         };
@@ -52,7 +59,8 @@ namespace GakuenMVC.Controllers
         // GET: ConfirmPayment/BuyList
         public ActionResult BuyList()
         {
-            return View(itemss);
+            ViewBag.Products = Products.ItemsList;
+            return View();
         }
 
         // POST: ConfirmPayment/Validate/5
