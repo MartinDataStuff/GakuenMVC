c124b575e5b10803f07f3caa0a7a4cef2b31c6fb
bashiradan <bashiradan75@gmail.com>
2016-12-01 15:25:47 +0100

    Fixed conflicts

--- DLLGakuen/Entity/User.cs
+++ DLLGakuen/Entity/User.cs
@@ -12,20 +12,21 @@ namespace DLLGakuen.Entity
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public bool Confirmed { get; set; }
+        public string PaidStringCode { get; set; }
         public string PhoneNr { get; set; }
         public Address Address { get; set; }
+        public DateTime Birthday { get; set; }
+        public int ContactPersonPhoneNumber { get; set; }
 
         //Login information
-        public string UserName { get; set; }
+        public string UsrName { get; set; }
         public string Password { get; set; }
         //Whether the user has accepted the confirm email
         public bool ConfirmedUser { get; set; }
 
 
-        //Shop oriented propaties
-        public List<OrderList> OrderLists { get; set; } = new List<OrderList>();
-
-        //School oriented propaties
+        
         public Schedule Schedule { get; set; }
         public enum Positions
         {
--- GakuenMVC/Controllers/AccountController.cs
+++ GakuenMVC/Controllers/AccountController.cs
@@ -161,7 +161,7 @@ namespace GakuenMVC.Controllers
                     ContactPersonPhoneNumber = model.ContactPersonPhoneNumber, Birthday = model.Birthday};
 
                 
-                var userSave = new User {Email = user.Email, UserName = user.UsrName, FirstName = user.FirstName, LastName = user.LastName,
+                var userSave = new User {Email = user.Email, UsrName = user.UsrName, FirstName = user.FirstName, LastName = user.LastName,
                     Password = user.Password, Address = user.Address, PhoneNr = user.PhoneNr, ContactPersonPhoneNumber = user.ContactPersonPhoneNumber,
                 Birthday = user.Birthday};
 
