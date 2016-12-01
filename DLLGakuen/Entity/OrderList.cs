e33d8584ea891248640313503141e58fecd27142
LifeLich <simo770g@easv365.dk>
2016-12-01 10:39:46 +0100

    Add entetys from API

---
+++ DLLGakuen/Entity/OrderList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLLGakuen.Entity
+{
+    public class OrderList : AbstractEntity
+    {
+        public List<Product> ItemsList { get; set; } = new List<Product>();
+        public User User { get; set; }
+        public string PaidStringCode { get; set; }
+        public double PriceToPay { get; set; }
+        public DateTime DateTime { get; set; } = DateTime.Now;
+    }
+}
